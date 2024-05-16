aa05843c939e2a2975e721b3392bc453b20faeb0
hokage-216 <berrylevente@gmail.com>
2024-05-16 13:18:33 -0400

    adding functionality to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,77 @@
+import React, { useRef, useState } from 'react';
+
 export default function Contact() {
-    return (
-      <div className="mt-5">
-        <h1 className="text-center">Contact Page</h1>
-        <p>
-          Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-          molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-          magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-          efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-          mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-          posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-          faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-          ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-          dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-          conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-          rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-        </p>
-      </div>
-    );
-  }
+  const fnameRef = useRef(null);
+  const lnameRef = useRef(null);
+  const emailRef = useRef(null);
+  const messageRef = useRef(null);
+  const [formError, setFormError] = useState('');
+
+  const checkFields = () => {
+    const fname = fnameRef.current.value.trim();
+    const lname = lnameRef.current.value.trim();
+    const email = emailRef.current.value.trim();
+    const message = messageRef.current.value.trim();
+
+    if (fname === '' && lname === '' && email === '' && message === '') {
+      setFormError('Please fill out the entire form before submitting!');
+    } else {
+      setFormError('');
+    }
+  };
+
+  return (
+    <div className="mt-5">
+      <h1 className="text-center">Contact Page</h1>
+      <form className="d-flex flex-column mt-5" action="#" id="contact-form">
+        <div className="d-flex flex-wrap">
+          <div className="d-flex flex-column p-2">
+            <label htmlFor="fname">First Name:</label>
+            <input
+              type="text"
+              name="fname"
+              id="fname"
+              ref={fnameRef}
+              onBlur={checkFields}
+            />
+          </div>
+          <div className="d-flex flex-column p-2">
+            <label htmlFor="lname">Last Name:</label>
+            <input
+              type="text"
+              name="lname"
+              id="lname"
+              ref={lnameRef}
+              onBlur={checkFields}
+            />
+          </div>
+        </div>
+        <div className="d-flex flex-column p-2">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            ref={emailRef}
+            onBlur={checkFields}
+          />
+        </div>
+        <div className="d-flex flex-column p-2">
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="message"
+            id="message"
+            cols="50"
+            rows="5"
+            ref={messageRef}
+            onBlur={checkFields}
+          ></textarea>
+        </div>
+        <div className="d-flex flex-column align-items-center">
+          {formError && <div>{formError}</div>}
+          <button type="submit" className="btn btn-primary p-2 col-3 mt-2">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
