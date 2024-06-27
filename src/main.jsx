32cfcd7b7d591bee7fbd2925aea7b45efe37a9c4
LeVente Berry <berrylevente@gmail.com>
2024-06-26 21:19:28 -0400

    changing loading of css from main.jsx to index.html

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import './index.css'
-
 import App from './App.jsx'
 import About from './pages/About.jsx'
 import Contact from './pages/Contact.jsx'
