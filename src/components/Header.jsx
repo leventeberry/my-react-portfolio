1f40b5583511b389077f694725a79f2dec0e3848
LeVente Berry <berrylevente@gmail.com>
2024-06-27 20:41:58 -0400

    final configuration for images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ export default function Header () {
     
     return (
         <nav className='navbar navbar-expand-lg d-flex' id='navbar'>
-            <img src="src/assets/TM-Logo-50.png" alt="Portfolio Logo"/>
-            <a id='nav-title' href="/">LeVente Berry</a>
+            <img src="/TM-Logo-50.png" alt="Portfolio Logo"/>
+            <a id='nav-title' href="/Home">LeVente Berry</a>
             <button className="navbar-toggler me-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,28 +3,28 @@ export default function Project() {
         {
           id: 1,
           title: "Cryptoverse Blog",
-          imgSrc: "src\\assets\\cryptoverse.png",
+          imgSrc: "/cryptoverse.png",
           deployedLink: "https://cryptoverse-blog-5710e4b7a1ef.herokuapp.com/home",
           githubLink: "https://github.com/hokage-216/cryptoverse-blog?tab=readme-ov-file"
         },
         {
           id: 2,
           title: "Urban Bassoon",
-          imgSrc: "src\\assets\\jate.png",
+          imgSrc: "/jate.png",
           deployedLink: "https://github.com/hokage-216/urban-bassoon",
           githubLink: "https://github.com/hokage-216/urban-bassoon"
         },
         {
           id: 3,
           title: "U-To-Do",
-          imgSrc: "src\\assets\\utodo.png",
+          imgSrc: "/utodo.png",
           deployedLink: "https://u-to-do.onrender.com",
           githubLink: "https://github.com/hokage-216/U-To-Do"
         },
         {
           id: 4,
           title: "CryptoMania",
-          imgSrc: "src\\assets\\cryptomania.png",
+          imgSrc: "/cryptomania.png",
           deployedLink: "https://cryptomania-ateam.netlify.app/",
           githubLink: "https://github.com/hokage-216/the-A-team"
         },
