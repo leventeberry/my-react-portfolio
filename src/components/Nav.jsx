2e880e815273fc13ed3b40398d77fbd11db93e8d
LeVente Berry <berrylevente@gmail.com>
2024-09-06 16:55:53 -0400

    updated structure

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import { Link, useLocation } from 'react-router-dom';
 
-export default function NavTabs() {
+export default function Nav() {
     const currentPage = useLocation().pathname;
   
     return (
